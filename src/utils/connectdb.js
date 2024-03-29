65f2ad585a0326562db27b7b85cf8f7ab200cdbd
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-03-30 02:11:34 +0700

    Revert "server: change connect string for upload host"
    This reverts commit d5101d7c4ed3dda97192b124062dc7df3ffa167c.

--- src/utils/connectdb.js
+++ src/utils/connectdb.js
@@ -1,19 +1,14 @@
 const mongoose = require("mongoose");
 
-let connectionString = "";
-if (process.env.NODE_ENV && process.env.NODE_ENV.includes("production")) {
-    connectionString = "mongodb+srv://hieutnbedev:2FNz4Q9nmvWw7mHm@memory-game-v2.5dekfuw.mongodb.net/?retryWrites=true&w=majority&appName=memory-game-v2"
-} else {
-    connectionString = "mongodb://localhost:27017/memory-game"
-}
-
+const connectString = "mongodb://localhost:27017/memory-game"
+// let connectString = "mongodb+srv://hieutnbedev:2FNz4Q9nmvWw7mHm@memory-game-v2.5dekfuw.mongodb.net/?retryWrites=true&w=majority&appName=memory-game-v2"
 const connect = async () => {
-    try {
-        await mongoose.connect(connectionString);
-        console.log("Connect DB successfully");
-    } catch (e) {
-        console.log("Connect DB failure");
-    }
+	try {
+		await mongoose.connect(connectString);
+		console.log("Connect DB successfully");
+	} catch (e) {
+		console.log("Connect DB failure");
+	}
 };
 
 module.exports = { connect };
